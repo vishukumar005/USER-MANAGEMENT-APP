690bf3585fd738b58637556e1b99ebdb96460df9
Vishal Kumar Singh <vs929263@gmail.com>
2025-07-08 15:15:41 +0530

    second

--- src/app/components/usercard/usercard.component.ts
+++ src/app/components/usercard/usercard.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-usercard',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './usercard.component.html',
   styleUrl: './usercard.component.css'
 })
 export class UsercardComponent {
 
+  @Input() user!: User;
+  isEditModalOpen: boolean = false;
+  editUser: User = new User({...this.user});
+
+  constructor(private UserService: UserService) {}
+  onEdit() {
+    this.editUser = new User({...this.user});
+    this.isEditModalOpen = true;
+  }
+
+  closeModal() {
+    this.isEditModalOpen = false;
+  }
+
+  saveChanges() {
+    this.UserService.editUserById(this.user.id, this.editUser);
+    this.isEditModalOpen = false;
+  }
+
+  onDelete() {
+    this.UserService.deleteUser(this.user.id);
+  }
 }
